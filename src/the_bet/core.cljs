f08b5e284d3f9112d7de9f40b29e9f46f97f8620
Dan Fornika <dfornika@gmail.com>
2020-08-21 19:53:16 -0700

    Add a couple rows to table

--- src/the_bet/core.cljs
+++ src/the_bet/core.cljs
@@ -3,7 +3,8 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]            
             [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]]))
+            [cljs.core.async :refer [<!]]
+            [clojure.pprint :as pprint]))
 
 (defonce app-db (r/atom {}))
 
@@ -11,9 +12,30 @@
   [:header
    [:h1 "The Bet"]])
 
+(defn table []
+  [:table {:class "table"}
+   [:tbody
+    [:tr
+     [:th "Year"]
+     [:th "Champion"]
+     [:th "Prize"]]
+    [:tr
+     [:td "1996"]
+     [:td "Detroit Red Wings"]
+     [:td "$2.00"]]
+    [:tr
+     [:td "1998"]
+     [:td "Detroit Red Wings"]
+     [:td "$4.00"]]
+    [:tr
+     [:td "1999"]
+     [:td "Dallas Stars"]
+     [:td "$10.00"]]]])
+
 (defn root []
   [:div
-   [header]])
+   [header]
+   [table]])
 
 (rdom/render [root] (js/document.getElementById "app"))
 
@@ -23,6 +45,11 @@
   
   (go (let [response (<! (http/get (str nhl-api-base "teams/1") {:with-credentials? false}))]
         (prn (get-in response [:body :teams 0 :name]))))
+
+  (go (let [response (<! (http/get (str nhl-api-base "game/1999030411/boxscore/") {:with-credentials? false}))]
+        (do
+          (pprint/pprint (str "Home team: " (get-in response [:body :teams :home :team :name])))
+          (pprint/pprint (str "Away team: " (get-in response [:body :teams :away :team :name]))))))
 
   (go (<! (http/get (str nhl-api-base "/teams/1"))))
   )
