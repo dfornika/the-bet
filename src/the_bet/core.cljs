c481f80ba0dc313537ca7a8b1e5d5fdbc7e4d7bc
Dan Fornika <dfornika@gmail.com>
2020-08-22 17:17:03 -0700

    Build table from data

--- src/the_bet/core.cljs
+++ src/the_bet/core.cljs
@@ -4,43 +4,69 @@
             [reagent.dom :as rdom]            
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
-            [clojure.pprint :as pprint]))
+            [clojure.pprint :as pprint]
+            [goog.string :as gstring]))
+
+(def yearly-winner-data [
+                         {:year 1997
+                          :champion "Detroit Red Wings"
+                          :prize 200
+                          :winner "Jeff"}
+                         {:year 1998
+                          :champion "Detroit Red Wings"
+                          :prize 400
+                          :winner "Jeff"}
+                         {:year 1999
+                          :champion "Dallas Stars"
+                          :prize 1000
+                          :winner "Dan"}
+                         {:year 2000
+                          :champion "New Jersey Devils"
+                          :prize 2000
+                          :winner "Jeff"}
+                         ])
 
 (defonce app-db (r/atom {}))
+
+(defn cents-to-dollar-string [cents]
+  (str "$" (str (gstring/format "%.2f" (/ cents 100)))))
 
 (defn header []
   [:header
    [:h1 "The Bet"]])
 
-(defn table []
-  [:table {:class "ui celled table"}
-   [:thead
-    [:tr
-     [:th "Year"]
-     [:th "Champion"]
-     [:th "Prize"]
-     [:th "Winner"]]]
-   [:tbody
-    [:tr
-     [:td "1997"]
-     [:td "Detroit Red Wings"]
-     [:td "$2.00"]
-     [:td "Jeff"]]
-    [:tr
-     [:td "1998"]
-     [:td "Detroit Red Wings"]
-     [:td "$4.00"]
-     [:td "Jeff"]]
-    [:tr
-     [:td "1999"]
-     [:td "Dallas Stars"]
-     [:td "$10.00"]
-     [:td "Dan"]]]])
+(defn description []
+  [:div
+   [:p "Every year I make a bet with a friend over the Stanley Cup playoffs. We started in 1997 and set the stakes at $2. The prize doubled each year until it reached $100."]
+   [:h2 "Rules"]
+   [:p "At the start of each playoff season, we take turns picking teams. The loser of the previous year chooses first. Whoever has the Stanley Cup Champion team wins $100."]])
+
+(defn table-headers [table-data]
+  (distinct (flatten (map keys table-data))))
+
+(defn table-rows [table-data]
+  (map (partial map str) (map vals table-data)))
+
+(defn create-body-row [row]
+  (vec (concat [:tr] (map #(conj [:td] %) row))))
+
+(defn data-to-table [table-data]
+  (let [header-row (vec (concat [:tr] (map #(conj [:th] %) (table-headers table-data))))
+        body-rows (vec (map create-body-row (table-rows table-data)))]
+    [:table {:class "ui celled table"}
+     [:thead header-row]
+     (into [:tbody] body-rows)]))
+
+
+(defn annual-summary []
+  (let [table-data (map #(update % :prize cents-to-dollar-string) yearly-winner-data)]
+    (data-to-table table-data)))
 
 (defn root []
   [:div
    [header]
-   [table]])
+   [description]
+   [annual-summary]])
 
 (rdom/render [root] (js/document.getElementById "app"))
 
